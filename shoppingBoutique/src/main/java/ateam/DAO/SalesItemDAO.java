bfc1768088cc2da5c34df9d191d5d542a64afd35
Terence Ramovha <131148766+terry-bytes@users.noreply.github.com>
2024-07-11 10:41:07 +0200

    retrieve pruduct details by scanning , modified the products dao and impl , will add other methods .. also restructured the sale and sales items dao and impl and added saleservice ..

--- shoppingBoutique/src/main/java/ateam/DAO/SalesItemDAO.java
+++ shoppingBoutique/src/main/java/ateam/DAO/SalesItemDAO.java
@@ -1,18 +1,11 @@
 package ateam.DAO;
 
-
 import ateam.Models.SalesItem;
 import java.util.List;
 
 public interface SalesItemDAO {
 
-    boolean createSalesItem(SalesItem salesItem);
+    boolean addItemToSale(int salesId, SalesItem item);
 
-    SalesItem getSalesItemById(int sales_item_ID); 
-
-    List<SalesItem> getSalesItemsBySaleId(int sales_ID); 
-
-    boolean updateSalesItem(SalesItem salesItem);
-
-    boolean deleteSalesItem(int sales_item_ID); 
+    boolean removeSaleItem(int salesItemId);
 }
