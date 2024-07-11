b4d480f9ddf6699cc6bacfbef46db1dabae20301
Terence Ramovha <131148766+terry-bytes@users.noreply.github.com>
2024-07-11 11:19:26 +0200

    added the storedaoimpl

---
+++ shoppingBoutique/src/main/java/ateam/DAOIMPL/StoreDAOIMPL.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ateam.DAOIMPL;
+
+import ateam.DAO.StoreDAO;
+import ateam.Models.Store;
+import java.util.List;
+
+/**
+ *
+ * @author Train
+ */
+public class StoreDAOIMPL implements StoreDAO{
+
+    @Override
+    public boolean addStore(Store store) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Store getStoreById(int store_ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Store> getAllStores() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateStore(Store store) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteStore(int store_ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
