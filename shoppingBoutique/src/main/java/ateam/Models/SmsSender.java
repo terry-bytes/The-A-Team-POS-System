0446021ba1f5c04ca2b1258d7feebe20f00721e3
Phumudzo Ramovha <17011162@mvula.univen.ac.za>
2024-07-28 19:49:54 +0200

    sms working, tested in the productServlet (sending saleDate) . added a dependency , build with dependencies before running

--- shoppingBoutique/src/main/java/ateam/Models/SmsSender.java
+++ shoppingBoutique/src/main/java/ateam/Models/SmsSender.java
@@ -10,9 +10,9 @@ import com.twilio.type.PhoneNumber;
 
 public class SmsSender {
 
-    public static final String ACCOUNT_SID = "Acc SID";
-    public static final String AUTH_TOKEN = "TOKEN";
-    public static final String TWILIO_PHONE_NUMBER = "NUMBER";
+    public static final String ACCOUNT_SID = "ACe8fdada7646497ac53040670783f2f5d";
+    public static final String AUTH_TOKEN = "4eb5d4746fd78e8d5dc54f6fb8187f49";
+    public static final String TWILIO_PHONE_NUMBER = "+12295446517";
 
     static {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
@@ -28,6 +28,6 @@ public class SmsSender {
         System.out.println("SMS sent successfully. SID: " + message.getSid());
     }
     public static void main(String[] args) {
-        sendSms("+27631821265", "the A team , collect your order ");
+        sendSms("+27631821265", "the A team , collect your order");
     }
 }
