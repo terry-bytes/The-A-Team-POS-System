0ca48238756487fcec9de39100adfe7bb355c926
Train 06 <Train 06@Studio21-18>
2024-08-05 10:16:46 +0200

    product servlet update

--- shoppingBoutique/src/main/java/ateam/Servlets/ProductServlet.java
+++ shoppingBoutique/src/main/java/ateam/Servlets/ProductServlet.java
@@ -275,8 +275,9 @@ public class ProductServlet extends HttpServlet {
                             String salespersonName = loggedInUser.getFirstName() + " " + loggedInUser.getLastName();
                             String saleTime = newSale.getSales_date().toString();
                             String customerEmail = request.getParameter("customer_email");
+                             request.setAttribute("saleID", newSalesID);
 
-                            emailService.sendSaleReceipt(customerEmail, salespersonName, saleTime, scannedItems, totalAmountWithoutVAT, vatAmount, change, newSale.getPayment_method(), cashPaid, cardPaid);
+                           emailService.sendSaleReceipt(customerEmail, salespersonName, saleTime, scannedItems, totalAmountWithoutVAT, vatAmount, change, newSale.getPayment_method(), cashPaid, cardPaid, newSalesID);
 
                             SmsSender.sendSms("+27631821265", "Thank you for SHOPPING with us! 😊 Please check your email (" + customerEmail + ") for your RECEIPT.");
 
