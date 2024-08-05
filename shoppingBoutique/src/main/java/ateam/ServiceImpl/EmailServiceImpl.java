b5777782112c0cc1c34b69a1a770ebf66539209d
Train 06 <Train 06@Studio21-18>
2024-08-05 09:57:47 +0200

    mail fix

--- shoppingBoutique/src/main/java/ateam/ServiceImpl/EmailServiceImpl.java
+++ shoppingBoutique/src/main/java/ateam/ServiceImpl/EmailServiceImpl.java
@@ -313,18 +313,6 @@ public class EmailServiceImpl implements EmailService {
             document.add(new Paragraph("Best regards,", normalFont));
             document.add(new Paragraph("Your Company Name", normalFont));
 
-            // Add Google Form link
-// Add Google Form link
-            String googleFormURL = "https://docs.google.com/forms/d/1MSLxiI52-CQt369KCBjfVfxGBPJyDa0_D7MDYGYCEng/viewform?pli=1&pli=1&edit_requested=true#responses";
-            Paragraph feedbackParagraph = new Paragraph();
-            feedbackParagraph.setSpacingBefore(10);
-            feedbackParagraph.add(new Chunk("We value your feedback. Please ", normalFont));
-            Chunk link = new Chunk("click here", normalFont);
-            link.setAction(new PdfAction(PdfAction.URL, googleFormURL));
-            feedbackParagraph.add(link);
-            feedbackParagraph.add(new Chunk(" to rate your experience.", normalFont));
-            document.add(feedbackParagraph);
-
             document.close();
             pdfStream.flush();
             byte[] pdfBytes = pdfStream.toByteArray();
